b4c7197738529a943da9d5b9c7d2685311db8efd
Abraham <oscara1706cl@gmail.com>
2019-09-21 14:52:27 -0500

    Hello

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import TodoForm from "./TodoForm.js";
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 
 function App() {
-  return <h1>Hello World</h1>
+  return(
+    <Fragment>
+      <Typography variant= 'h2' align='center' gutterBottom>
+        Lista de cosas por hacer
+      </Typography>
+      <Grid container justify="center">
+        <Grid item >
+            <TodoForm/>
+        </Grid>
+      </Grid>
+      <Grid container justify="center">
+        <Grid item md={8}>
+          {/* <TodoList
+            todos={this.state.todos}
+            deleteTodo={this.deleteTodo}
+            isItCheckedBox={this.isItCheckedBox}/>*/}
+        </Grid>
+      </Grid>
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/TodoForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import * as actions from "./redux/actions";
+import {connect} from "react-redux";
+
+
+
+const TodoForm= ({value, updateValue}) =>{
+  return(
+    <form
+      onSubmit={e =>{
+        e.preventDefault();
+      }}>
+      <TextField
+        label="To do"
+         type="text"
+          placeholder="Next to do...."
+          value={value}
+          onChange = {e => updateValue(e.target.value)}
+
+          />
+
+    </form>
+  );
+}
+
+const mapStateToProps = ({todoReducer}) =>{
+  const {value} = todoReducer;
+  return {value}
+}
+
+export default connect(
+  mapStateToProps,
+  actions
+)(TodoForm);
