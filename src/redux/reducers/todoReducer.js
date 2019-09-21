bc4f19d3bad87573f37bbb8ec6c8275d40a14061
Abraham <oscara1706cl@gmail.com>
2019-09-21 14:30:39 -0500

    Creando app redux 0

---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,22 @@
+import {UPDATE_VALUE} from "../actions/actionTypes"
+
+
+const INITIAL_STATE = {
+  value: "",
+  todos: []
+}
+
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case UPDATE_VALUE:
+          return{
+            ...state,
+            value: action.payload
+          }
+        break;
+      default:
+        return state;
+
+    }
+}
